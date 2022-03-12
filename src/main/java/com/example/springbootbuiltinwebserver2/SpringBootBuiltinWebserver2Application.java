5d1b3eb6c6c29684f6299463942ef43f840592c6
jsj3282 <jeongseonju15@gmail.com>
2022-03-12 23:20:27 +0900

    first commit

---
+++ src/main/java/com/example/springbootbuiltinwebserver2/SpringBootBuiltinWebserver2Application.java
@@ -0,0 +1,40 @@
+package com.example.springbootbuiltinwebserver2;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootBuiltinWebserver2Application {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBuiltinWebserver2Application.class, args);
+    }
+
+//    // Additional HTTP
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//        return tomcat;
+//    }
+//
+//    private Connector createStandardConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setPort(8080);
+//        return connector;
+//    }
+
+
+}
---
+++ src/test/java/com/example/springbootbuiltinwebserver2/SpringBootBuiltinWebserver2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbuiltinwebserver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBuiltinWebserver2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
